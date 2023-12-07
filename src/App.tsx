e4b3cf0dbda340ec238f6d479a68b89290b051ce
Nino Nakano <yudhaprasty17@gmail.com>
2023-12-08 02:56:43 +0700

    micro-feature commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MobileLegend from "./assets/Components/MobileLegends"
+
+export default function App(){
+  return(
+    <>
+      <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+        <MobileLegend />
+      </div>
+    </>
+  )
+}
---
+++ src/assets/Components/MobileLegends.tsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
+import '../css/MobileLegend.css'
+import axios from 'axios'
+
+interface hero {
+  hero_id: number
+  hero_name: string
+  hero_avatar: string
+  hero_role: string
+  hero_specially: string
+}
+
+function MobileLegend() {
+  const [heroList, setHeroList] = useState<hero[]>()
+  const [text, setText] = useState('')
+
+  useEffect(() => {
+    loadHeroData()
+  }, [text])
+
+  const loadHeroData = async () => {
+    return await axios
+        .get('https://api.dazelpro.com/mobile-legends/hero')
+        .then(res => setHeroList(res.data.hero))
+        .catch(err => console.log(err))
+   }
+
+  const handleSearch = () => {
+    const findHero = heroList?.filter((u) => u?.hero_name.toLowerCase().includes(text.toLowerCase()))
+    setHeroList(findHero)
+  }
+  
+  const handleReset = () => {
+    loadHeroData()
+    setText('')
+  }
+
+  return (
+    <>
+        <h2 style={{margin:50}}>Daftar Hero Mobile Legend</h2>
+        <div style={{marginTop:10}}>
+        <InputGroup>
+            <Form.Control
+            placeholder="Cari hero berdasarkan nama" value={text} onChange={(e) => setText(e.target.value)}
+            />
+            <Button variant="outline-secondary" onClick={() => handleSearch()}>Cari</Button>
+            <Button variant="outline-secondary" onClick={() => handleReset()}>Reset</Button>
+        </InputGroup>
+        {heroList?.map(hero => (
+          <div className='heroContainer' key={hero.hero_id}>
+            <h5 style={{margin:10, paddingLeft:20, paddingTop:25}}>{hero.hero_name}</h5>
+            <p style={{paddingLeft:30, paddingBottom:25}}>
+            Peran: {hero.hero_role}<br></br>
+            Kemampuan: {hero.hero_specially}
+            </p>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default MobileLegend
