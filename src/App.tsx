f03eb20963f7ffa10749b377445dc648d09fa9dd
Nino Nakano <yudhaprasty17@gmail.com>
2023-12-08 06:07:19 +0700

    micro-feature commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import CoundownTime from "./assets/Components/CountdownTime"
+
+export default function App(){
+  return(
+    <>
+      <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+        <CoundownTime />
+      </div>
+    </>
+  )
+}
